0d6e7c22f1442182e339a9eac620329c4893855d
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-20 19:53:56 +0900

    商品情報を保存する実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :price, :category_id, :item_status_id, :delivery_price_id, :prefecture_id, :delivery_criterion_id, :image).merge(user_id: current_user.id)
+end
 end
