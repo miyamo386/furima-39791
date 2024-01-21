86dae0e3796f52acbf762f5bfd9c05fd508333cb
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-21 15:09:23 +0900

    itemモデル単体テストの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+    item_name { '三色ボールペン' }
+    item_description { '赤・青・黒インクの発色が良く書き味がサラサラして書きやすいです' }
+    price { '500' }
+    category { Category.all.sample }
+    item_status { ItemStatus.all.sample }
+    delivery_price { DeliveryPrice.all.sample }
+    prefecture { Prefecture.all.sample }
+    delivery_criterion { DeliveryCriterion.all.sample }
   end
 end
