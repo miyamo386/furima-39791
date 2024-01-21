37fb7c948f01c759ec28d4700e80ad2dc68ea7e4
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-21 20:38:01 +0900

    JavaScriptで販売価格の詳細を実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,2 +1,12 @@
 window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const handlingCharge = document.getElementById("add-tax-price");
+    handlingCharge.innerHTML = Math.floor(inputValue * 0.1)
+    const benefit = document.getElementById("profit");
+    console.log(benefit);
+    benefit.innerHTML = Math.floor(inputValue - handlingCharge.innerHTML)
+    
+  })
 });
