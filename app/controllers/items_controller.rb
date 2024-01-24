525a1aa10ab82581bf08395b686f2c19d20b9aba
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-24 15:38:34 +0900

    コントローラーにshowアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :set_item, except: [:index, :new, :create]
 
   def index
     @item = Item.all.order('created_at DESC')
@@ -18,10 +19,18 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+  
   private
 
   def item_params
     params.require(:item).permit(:item_name, :item_description, :price, :category_id, :item_status_id, :delivery_price_id,
                                  :prefecture_id, :delivery_criterion_id, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
