2afe98274cd0a1dee3711e921a840df93251337b
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-25 18:30:31 +0900

    move_to_indexメソッド修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,8 +46,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return unless user_signed_in?
-
-    redirect_to action: :index
+    if user_signed_in? && current_user.id == @item.user
+      redirect_to action: :index
+    end
   end
 end
