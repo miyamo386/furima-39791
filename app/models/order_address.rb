5caa7a6aeb9c5c530f02bd432910e8197ba13ce2
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-26 20:13:00 +0900

    orderとAddressの情報をテーブルに保存

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,4 +9,9 @@ class OrderAddress
     validates :block
     validates :phone_number, numericality: { only_integer: true }, length: { in: 10..11, message: "Enter between 10 and 11 digits"}
   end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, user_id: user_id, item_id: item_id)
+  end
 end
