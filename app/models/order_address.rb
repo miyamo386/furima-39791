ea8356dec9fd4559cd6d3c693d985e9096fda6df
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-27 21:40:21 +0900

    エラーメッセージの表示追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,7 +4,7 @@ class OrderAddress
 
   with_options presence: true do
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
     validates :city
     validates :block
     validates :phone_number, numericality: { only_integer: true }, length: { in: 10..11, message: "Enter between 10 and 11 digits"}
