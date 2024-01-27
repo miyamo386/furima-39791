7781cb74826558845cad7e9a99c543984414f44f
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-27 22:02:03 +0900

    order_addressのFactoryBot設定

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :order_address do
-    
+    postcode { '123-4567' }
+    prefecture_id { 2 }
+    city { '東京都' }
+    block { '1-1' }
+    building { '東京ハイツ' }
+    phone_number { 11122223333 }
   end
 end
