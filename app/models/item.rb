758b62ccf1475fc660cc76d227b12e9d4e47e354
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-27 17:06:19 +0900

    itmeモデルのアソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  belongs_to :order
+  has_one :order
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
