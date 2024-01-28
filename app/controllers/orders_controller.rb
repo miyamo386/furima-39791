c999c5399c64c2ac94ea7646cfe2efe4f59be329
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-28 16:49:05 +0900

    ordersコントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, only: :create
+  before_action :set_item, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -18,6 +19,10 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
