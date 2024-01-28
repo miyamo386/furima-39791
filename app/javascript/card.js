afa0075430fb17645df7f9b79e56af462ca7bfd8
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-28 21:20:41 +0900

    カード情報をJavaScriptで取得

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,4 +1,13 @@
 const pay = () => {
+
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
     console.log("フォーム送信時にイベント発火")
