c4ecb18c08f93491510548e06a38bf7495469af2
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-30 22:36:27 +0900

    ログインや商品売買状態でボタン表示変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
   
   def index
-    if current_user.id != @item.user.id && @item.order.present?
+    if current_user.id != @item.user.id && @item.order.blank?
       gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       @order_address = OrderAddress.new
     else
