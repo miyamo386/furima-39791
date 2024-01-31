caca000b880806d3445b507a3a306bc82de79ad8
miyamo386 <xfttc581@yahoo.co.jp>
2024-01-31 17:00:11 +0900

    Formオブジェク単体テストにitを追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   before do
     user = FactoryBot.create(:user)
-    @order_address = FactoryBot.build(:order_address, user_id: user.id)
+    item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
   end
 
   describe '配送先情報の保存' do
@@ -83,6 +84,18 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.token = nil
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+
+      it 'user_idが空だと登録できない' do
+        @order_address.user_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'item_idが空だと登録できない' do
+        @order_address.item_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
       end
     end
   end
