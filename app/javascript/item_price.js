64152fbe5910496d69d1c59801984e9231ffe9f7
miyamo386 <xfttc581@yahoo.co.jp>
2024-02-01 21:42:34 +0900

    商品購入機能の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-window.addEventListener('turbo:load', () => {
+const price = () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
@@ -7,4 +7,7 @@ window.addEventListener('turbo:load', () => {
     const benefit = document.getElementById("profit");
     benefit.innerHTML = Math.floor(inputValue - handlingCharge.innerHTML)
   })
-});
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
